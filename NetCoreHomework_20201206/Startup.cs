4407b635073ddd8d9dcaca0226ba7a579eb7a437
david.chen <david@clacomusic.com>
2020-12-06 21:56:24 +0800

    以 ContosoUniversity 資料庫為主要資料來源，並透過 DB First 流程建立 EF Core 實體資料模型。

--- NetCoreHomework_20201206/Startup.cs
+++ NetCoreHomework_20201206/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NetCoreHomework_20201206.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,10 @@ namespace NetCoreHomework_20201206
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // using Microsoft.EntityFrameworkCore;
+            services.AddDbContext<ContosoUniversityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
