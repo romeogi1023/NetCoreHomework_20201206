40dac94a5e3e31c3fe2a88240918ab474b866b23
david.chen <david@clacomusic.com>
2020-12-06 22:10:43 +0800

    須對資料表 Course、Department、OfficeAssignment、Person 設計出完整的 CRUD 操作 APIs (透過VS2019控制自動產生)

---
+++ NetCoreHomework_20201206/Controllers/DepartmentsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreHomework_20201206.Models;
+
+namespace NetCoreHomework_20201206.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly ContosoUniversityContext _context;
+
+        public DepartmentsController(ContosoUniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartment()
+        {
+            return await _context.Department.ToListAsync();
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return department;
+        }
+
+        // PUT: api/Departments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment(int id, Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Departments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Department>> PostDepartment(Department department)
+        {
+            _context.Department.Add(department);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDepartment", new { id = department.DepartmentId }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _context.Department.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return _context.Department.Any(e => e.DepartmentId == id);
+        }
+    }
+}
---
+++ NetCoreHomework_20201206/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreHomework_20201206.Models;
+
+namespace NetCoreHomework_20201206.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfficeAssignmentsController : ControllerBase
+    {
+        private readonly ContosoUniversityContext _context;
+
+        public OfficeAssignmentsController(ContosoUniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OfficeAssignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OfficeAssignment>>> GetOfficeAssignment()
+        {
+            return await _context.OfficeAssignment.ToListAsync();
+        }
+
+        // GET: api/OfficeAssignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> GetOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignment.FindAsync(id);
+
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return officeAssignment;
+        }
+
+        // PUT: api/OfficeAssignments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOfficeAssignment(int id, OfficeAssignment officeAssignment)
+        {
+            if (id != officeAssignment.InstructorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(officeAssignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OfficeAssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OfficeAssignments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OfficeAssignment>> PostOfficeAssignment(OfficeAssignment officeAssignment)
+        {
+            _context.OfficeAssignment.Add(officeAssignment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OfficeAssignmentExists(officeAssignment.InstructorId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOfficeAssignment", new { id = officeAssignment.InstructorId }, officeAssignment);
+        }
+
+        // DELETE: api/OfficeAssignments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignment.FindAsync(id);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.OfficeAssignment.Remove(officeAssignment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OfficeAssignmentExists(int id)
+        {
+            return _context.OfficeAssignment.Any(e => e.InstructorId == id);
+        }
+    }
+}
